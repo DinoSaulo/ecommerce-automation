f9f7f1939f64ff5904e74200e1261dc560578208
Saulo Barros <saulobarros1997@gmail.com>
2025-05-16 17:08:49 -0300

    fix 5

--- cypress.config.js
+++ cypress.config.js
@@ -20,8 +20,28 @@ module.exports = defineConfig({
     inlineAssets: true,
   },
   env: {
-    url: 'https://www.automationexercise.com/',
-    cartpageurl: 'https://www.automationexercise.com/view_cart'
+    "url": "https://www.automationexercise.com/",
+    "name": "test0128879123898",
+    "password": "test012890879123889",
+    "email" : "alexsmkovtkvtzb4979128883@sgisfg.com",
+    "wrong-password": "test01289087912377",
+    "wrong-email" : "alexsmkovtkvtzb497912377@sgisfg.com",
+    "testcasepageurl": "https://www.automationexercise.com/test_cases",
+    "productpageurl": "https://www.automationexercise.com/products",
+    "firstproductpageurl": "https://www.automationexercise.com/product_details/1",
+    "cartpageurl": "https://www.automationexercise.com/view_cart",
+    "name1": "tes791237845",
+    "password1": "test0128791237845",
+    "email1" : "alexsmkovtkv4979123784@sgisfg.com",
+    "polobrandpageurl": "https://www.automationexercise.com/brand_products/Polo",
+    "hnmbrandpageurl": "https://www.automationexercise.com/brand_products/H&M" ,
+    "name2": "tes7912378898",
+    "password2": "test01287912378898",
+    "email2" : "alexsmkovtkv4889849552@sgisfg.com",
+
+    "name3": "tes79123788987",
+    "password3": "test01287912378897",
+    "email3" : "alexsmkovtkv479849552@sgisfg.com"
   },
   e2e: {
     // We've imported your old cypress plugins here.
